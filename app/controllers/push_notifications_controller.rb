1994f652925f5db621665e9e002ae7a5127b1c39
sert <demo.sert@gmail.com>
2015-12-26 01:50:09 +0900

    add device type validation

--- app/controllers/push_notifications_controller.rb
+++ app/controllers/push_notifications_controller.rb
@@ -1,11 +1,26 @@
 class PushNotificationsController < ApplicationController
   def create
-    device = Device.new(device_token: params[:device_token])
+    unless valid_device_type?
+      render status: 422, json: { 'message': 'device type is invalid' }
+      return
+    end
+
+    device = Device.new(device_type: params[:device_type], device_token: params[:device_token])
 
     if device.save
       render status: 200, json: { 'id': device.id }
     else
-      render status: 422, json: { 'message': 'token is nil' }
+      render status: 422, json: { 'message': 'registration is failure' }
+    end
+  end
+
+  private
+
+  def valid_device_type?
+    if params[:device_type] != 'android' && params[:device_type] != 'ios'
+      false
+    else
+      true
     end
   end
 end
