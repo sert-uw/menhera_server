dfddfec859d9e473e454b656437eab0df61f5efe
sert <demo.sert@gmail.com>
2015-12-26 00:52:14 +0900

    run rake db:migrate

--- db/schema.rb
+++ db/schema.rb
@@ -11,6 +11,66 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 20151225155131) do
+
+  create_table "rpush_apps", force: :cascade do |t|
+    t.string   "name",                    limit: 255,               null: false
+    t.string   "environment",             limit: 255
+    t.text     "certificate",             limit: 65535
+    t.string   "password",                limit: 255
+    t.integer  "connections",             limit: 4,     default: 1, null: false
+    t.datetime "created_at"
+    t.datetime "updated_at"
+    t.string   "type",                    limit: 255,               null: false
+    t.string   "auth_key",                limit: 255
+    t.string   "client_id",               limit: 255
+    t.string   "client_secret",           limit: 255
+    t.string   "access_token",            limit: 255
+    t.datetime "access_token_expiration"
+  end
+
+  create_table "rpush_feedback", force: :cascade do |t|
+    t.string   "device_token", limit: 64, null: false
+    t.datetime "failed_at",               null: false
+    t.datetime "created_at"
+    t.datetime "updated_at"
+    t.integer  "app_id",       limit: 4
+  end
+
+  add_index "rpush_feedback", ["device_token"], name: "index_rpush_feedback_on_device_token", using: :btree
+
+  create_table "rpush_notifications", force: :cascade do |t|
+    t.integer  "badge",             limit: 4
+    t.string   "device_token",      limit: 64
+    t.string   "sound",             limit: 255,      default: "default"
+    t.string   "alert",             limit: 255
+    t.text     "data",              limit: 65535
+    t.integer  "expiry",            limit: 4,        default: 86400
+    t.boolean  "delivered",                          default: false,     null: false
+    t.datetime "delivered_at"
+    t.boolean  "failed",                             default: false,     null: false
+    t.datetime "failed_at"
+    t.integer  "error_code",        limit: 4
+    t.text     "error_description", limit: 65535
+    t.datetime "deliver_after"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+    t.boolean  "alert_is_json",                      default: false
+    t.string   "type",              limit: 255,                          null: false
+    t.string   "collapse_key",      limit: 255
+    t.boolean  "delay_while_idle",                   default: false,     null: false
+    t.text     "registration_ids",  limit: 16777215
+    t.integer  "app_id",            limit: 4,                            null: false
+    t.integer  "retries",           limit: 4,        default: 0
+    t.string   "uri",               limit: 255
+    t.datetime "fail_after"
+    t.boolean  "processing",                         default: false,     null: false
+    t.integer  "priority",          limit: 4
+    t.text     "url_args",          limit: 65535
+    t.string   "category",          limit: 255
+  end
+
+  add_index "rpush_notifications", ["app_id", "delivered", "failed", "deliver_after"], name: "index_rapns_notifications_multi", using: :btree
+  add_index "rpush_notifications", ["delivered", "failed"], name: "index_rpush_notifications_multi", using: :btree
 
 end
