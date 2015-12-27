38dfcf4e032148f7e6a933a43e44e437cdd888b5
柳 耕太 <yanagikouta0602@gmail.com>
2015-12-28 04:45:13 +0900

    :add ユーザ登録機能の追加
    ・routesファイルの編集
    ・user_controllerを作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,12 @@
 Rails.application.routes.draw do
   resources :push_notifications, only: [:create], defaults: { format: :json }
   resources :post_notifications, only: [:create], defaults: { format: :json }
+
+  resources post 'user',               only: [:create], defaults: { format: :json }
+  resources get 'girls',               only: [:index], defaults: { format: :json }
+  resources 'girls' do
+    resources 'messages'
+  end
+  resources get 'response_candidates', only: [:index], defaults: { format: :json }
+
 end
