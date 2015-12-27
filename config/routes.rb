f537e852dbdb467e67067aeb0972df765a12ea60
EC2 Default User <ec2-user@ip-172-31-24-8.ap-northeast-1.compute.internal>
2015-12-27 19:49:15 +0000

    fixup routing

--- config/routes.rb
+++ config/routes.rb
@@ -2,11 +2,11 @@ Rails.application.routes.draw do
   resources :push_notifications, only: [:create], defaults: { format: :json }
   resources :post_notifications, only: [:create], defaults: { format: :json }
 
-  resources post 'user',               only: [:create], defaults: { format: :json }
-  resources get 'girls',               only: [:index], defaults: { format: :json }
-  resources 'girls' do
+  resources 'users', only: [:create], defaults: { format: :json }
+
+  resources 'girls', only: [:index], defaults: { format: :json } do
     resources 'messages'
   end
-  resources get 'response_candidates', only: [:index], defaults: { format: :json }
 
+  resources 'response_candidates', only: [:index], defaults: { format: :json }
 end
