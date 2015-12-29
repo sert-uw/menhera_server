5982082ec60938e084d48b10d49071136a5ba509
sert <demo.sert@gmail.com>
2015-12-29 16:04:06 +0000

    create seed_fu fixtures

--- db/fixtures/04_message.rb
+++ db/fixtures/04_message.rb
@@ -1,107 +1,107 @@
 Message.seed do |m|
   m.id = 1
   m.from = Message::FROM[:girl]
-  m.text = '寂しい'
   m.read = false
   m.user_id = 1
   m.girl_id = 1
+  m.message_list_id = 1
 end
 
 Message.seed do |m|
   m.id = 2
   m.from = Message::FROM[:girl]
-  m.text = '寂しいよ...'
   m.read = false
   m.user_id = 1
   m.girl_id = 1
+  m.message_list_id = 2
 end
 
 Message.seed do |m|
   m.id = 3
   m.from = Message::FROM[:girl]
-  m.text = 'どうして返事くれないの?'
   m.read = false
   m.user_id = 1
   m.girl_id = 1
+  m.message_list_id = 3
 end
 
 Message.seed do |m|
   m.id = 4
   m.from = Message::FROM[:girl]
-  m.text = '会いたい'
   m.read = false
   m.user_id = 2
   m.girl_id = 1
+  m.message_list_id = 1
 end
 
 Message.seed do |m|
   m.id = 5
   m.from = Message::FROM[:girl]
-  m.text = '会いたい会いたい'
   m.read = false
   m.user_id = 2
   m.girl_id = 1
+  m.message_list_id = 2
 end
 
 Message.seed do |m|
   m.id = 6
   m.from = Message::FROM[:girl]
-  m.text = '...'
   m.read = false
   m.user_id = 2
   m.girl_id = 1
+  m.message_list_id = 3
 end
 
 Message.seed do |m|
   m.id = 7
   m.from = Message::FROM[:girl]
-  m.text = '死にたい'
   m.read = false
   m.user_id = 3
   m.girl_id = 1
+  m.message_list_id = 1
 end
 
 Message.seed do |m|
   m.id = 8
   m.from = Message::FROM[:girl]
-  m.text = 'さよなら'
   m.read = false
   m.user_id = 3
   m.girl_id = 1
+  m.message_list_id = 2
 end
 
 Message.seed do |m|
   m.id = 9
   m.from = Message::FROM[:girl]
-  m.text = '...'
   m.read = false
   m.user_id = 3
   m.girl_id = 1
+  m.message_list_id = 3
 end
 
 Message.seed do |m|
   m.id = 10
   m.from = Message::FROM[:girl]
-  m.text = 'ねぇ...なんでもない'
   m.read = false
   m.user_id = 4
   m.girl_id = 1
+  m.message_list_id = 1
 end
 
 Message.seed do |m|
   m.id = 11
   m.from = Message::FROM[:girl]
-  m.text = 'あのさぁ'
   m.read = false
   m.user_id = 4
   m.girl_id = 1
+  m.message_list_id = 2
 end
 
 Message.seed do |m|
   m.id = 12
   m.from = Message::FROM[:girl]
-  m.text = '...'
   m.read = false
   m.user_id = 4
   m.girl_id = 1
+  m.message_list_id = 3
 end
---
+++ db/fixtures/05_message_candidate.rb
@@ -0,0 +1,119 @@
+MessageListResponseCandidate.seed do |mr|
+  mr.id = 1
+  mr.message_list_id = 1
+  mr.response_candidate_id = 1
+  mr.dependence_point = 5
+  mr.next_message_id = 3
+end
+
+MessageListResponseCandidate.seed do |mr|
+  mr.id = 2
+  mr.message_list_id = 1
+  mr.response_candidate_id = 2
+  mr.dependence_point = 10
+  mr.next_message_id = 4
+end
+
+MessageListResponseCandidate.seed do |mr|
+  mr.id = 3
+  mr.message_list_id = 1
+  mr.response_candidate_id = 3
+  mr.dependence_point = -5
+  mr.next_message_id = 5
+end
+
+MessageListResponseCandidate.seed do |mr|
+  mr.id = 4
+  mr.message_list_id = 2
+  mr.response_candidate_id = 1
+  mr.dependence_point = 3
+  mr.next_message_id = 1
+end
+
+MessageListResponseCandidate.seed do |mr|
+  mr.id = 5
+  mr.message_list_id = 2
+  mr.response_candidate_id = 2
+  mr.dependence_point = 8
+  mr.next_message_id = 4
+end
+
+MessageListResponseCandidate.seed do |mr|
+  mr.id = 6
+  mr.message_list_id = 2
+  mr.response_candidate_id = 3
+  mr.dependence_point = -5
+  mr.next_message_id = 3
+end
+
+MessageListResponseCandidate.seed do |mr|
+  mr.id = 7
+  mr.message_list_id = 3
+  mr.response_candidate_id = 1
+  mr.dependence_point = -5
+  mr.next_message_id = 1
+end
+
+MessageListResponseCandidate.seed do |mr|
+  mr.id = 8
+  mr.message_list_id = 3
+  mr.response_candidate_id = 2
+  mr.dependence_point = 5
+  mr.next_message_id = 4
+end
+
+MessageListResponseCandidate.seed do |mr|
+  mr.id = 9
+  mr.message_list_id = 3
+  mr.response_candidate_id = 3
+  mr.dependence_point = -10
+  mr.next_message_id = 5
+end
+
+MessageListResponseCandidate.seed do |mr|
+  mr.id = 10
+  mr.message_list_id = 4
+  mr.response_candidate_id = 1
+  mr.dependence_point = -5
+  mr.next_message_id = 5
+end
+
+MessageListResponseCandidate.seed do |mr|
+  mr.id = 11
+  mr.message_list_id = 4
+  mr.response_candidate_id = 2
+  mr.dependence_point = 10
+  mr.next_message_id = 2
+end
+
+MessageListResponseCandidate.seed do |mr|
+  mr.id = 12
+  mr.message_list_id = 4
+  mr.response_candidate_id = 3
+  mr.dependence_point = -10
+  mr.next_message_id = 5
+end
+
+MessageListResponseCandidate.seed do |mr|
+  mr.id = 13
+  mr.message_list_id = 5
+  mr.response_candidate_id = 1
+  mr.dependence_point = -10
+  mr.next_message_id = 3
+end
+
+MessageListResponseCandidate.seed do |mr|
+  mr.id = 14
+  mr.message_list_id = 5
+  mr.response_candidate_id = 2
+  mr.dependence_point = 5
+  mr.next_message_id = 2
+end
+
+MessageListResponseCandidate.seed do |mr|
+  mr.id = 15
+  mr.message_list_id = 5
+  mr.response_candidate_id = 3
+  mr.dependence_point = -20
+  mr.next_message_id = 3
+end
