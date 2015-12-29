76bd33d78efb88c71af33ea60bfb58e78505290e
sert <demo.sert@gmail.com>
2015-12-29 13:42:12 +0000

    remove unnecessary devendence

--- app/models/message_list.rb
+++ app/models/message_list.rb
@@ -2,5 +2,4 @@ class MessageList < ActiveRecord::Base
   belongs_to :girl
 
   has_many :message_list_response_candidates
-  has_many :response_candidates, through: :message_list_response_candidates
 end
--- app/models/response_candidate.rb
+++ app/models/response_candidate.rb
@@ -1,5 +1,4 @@
 class ResponseCandidate < ActiveRecord::Base
   has_many :message_list_response_candidates
-  has_many :message_lists, through: :message_list_response_candidates
   belongs_to :girl
 end
