c16392d96cbe48c7c99e4bdac14431042280a9c6
sert <demo.sert@gmail.com>
2015-12-29 14:57:39 +0000

    add messages controller update

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -4,4 +4,11 @@ class MessagesController < BaseController
   def index
     @messages = Message.where(user_id: current_user.id, girl_id: params[:girl_id], from: Message::FROM[:girl], read: false)
   end
+
+  def update
+    messages = Message.where(user_id: current_user.id, girl_id: params[:girl_id], from: Message::FROM[:girl], read: false).where("id <= ?", params[:id])
+    messages.update_all(read: true)
+
+    render status:200, json: {}
+  end
 end
