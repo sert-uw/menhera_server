ea3ad5fefdbcd1032781976a332cf8b7315c72fa
sert <demo.sert@gmail.com>
2015-12-29 16:00:33 +0000

    add dependence

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,6 +1,7 @@
 class Message < ActiveRecord::Base
   belongs_to :user
   belongs_to :girl
+  belongs_to :message_list
 
   FROM = { user: 'user', girl: 'girl' }
 end
--- app/models/message_list.rb
+++ app/models/message_list.rb
@@ -1,5 +1,6 @@
 class MessageList < ActiveRecord::Base
   belongs_to :girl
+  has_many :messages
 
   has_many :message_list_response_candidates
 end
