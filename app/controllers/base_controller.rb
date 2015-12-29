7e2e2bc7aeaa6a1578050b578366256e502bd2fd
sert <demo.sert@gmail.com>
2015-12-29 10:00:47 +0000

    fixup

--- app/controllers/base_controller.rb
+++ app/controllers/base_controller.rb
@@ -2,7 +2,7 @@ class BaseController < ActionController::Base
   protected
 
     def authenticate_user!
-      @current_user = User.find token: params[:token] if params[:token].present?
+      @current_user = User.find_by token: params[:token] if params[:token].present?
 
       unless @current_user.present?
         render status: 422, json: { message: 'token is invalid' }
