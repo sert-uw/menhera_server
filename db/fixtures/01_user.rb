ceb2ca3e951ab9692095add966389c8e010995ac
sert <demo.sert@gmail.com>
2015-12-31 06:40:54 +0000

    fixup fixtures

---
+++ db/fixtures/01_user.rb
@@ -0,0 +1,11 @@
+User.seed do |u|
+  u.id = 1
+  u.name = 'メンヘラマスター'
+  u.uuid = '1234-1234-1234-1234'
+end
+
+User.seed do |u|
+  u.id = 2
+  u.name = 'メンヘラ入門'
+  u.uuid = 'abcd-abcd-abcd-abcd'
+end
