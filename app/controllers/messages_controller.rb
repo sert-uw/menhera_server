c70980905c7822b820b8467818806c7f93b7b42d
sert <demo.sert@gmail.com>
2015-12-31 06:41:06 +0000

    fixup controller

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,30 +1,36 @@
 class MessagesController < BaseController
   before_action :authenticate_user!, only: [:index, :create, :update]
+  before_action :set_user_girl, only: [:index, :create, :update]
 
   def index
-    @messages = MessageDecorator.decorate_collection(Message.where(user_id: current_user.id, girl_id: params[:girl_id], from: Message::FROM[:girl], read: false).includes(:message_list))
+    @messages = MessageDecorator.decorate_collection(@user_girl.messages.where(from: Message::FROM[:girl], read: false).includes(:message_list))
   end
 
   def create
-    candidate = ResponseCandidate.find params[:candidate_id]
-    last_message = Message.where(from: Message::FROM[:girl]).last
+    @user_girl.messages.create(from: Message::FROM[:user], response_candidate_id: params[:candidate_id])
 
-    Message.create(from: Message::FROM[:user], user_id: current_user.id, girl_id: params[:girl_id], response_candidate_id: candidate.text, read: true)
+    last_message = Message.where(from: Message::FROM[:girl]).last
 
     message_candidate = MessageListResponseCandidate.find_by(message_list_id: last_message.message_list_id, response_candidate_id: params[:candidate_id])
 
     user_girl = current_user.user_girls.find(params[:girl_id])
     user_girl.dependence += message_candidate.dependence_point
 
-    Message.create(from: Message::FROM[:girl], user_id: current_user.id, girl_id: params[:girl_id], message_list_id: message_candidate.next_message_id)
+    @user_girl.messages.create(from: Message::FROM[:girl], message_list_id: message_candidate.next_message_id)
 
     render status: 200, json: {}
   end
 
   def update
-    messages = Message.where(user_id: current_user.id, girl_id: params[:girl_id], from: Message::FROM[:girl], read: false).where("id <= ?", params[:id])
+    messages = @user_girl.messages.where(from: Message::FROM[:girl], read: false).where("id <= ?", params[:id])
     messages.update_all(read: true)
 
     render status:200, json: {}
+  end
+
+  private
+
+  def set_user_girl
+    @user_girl = current_user.user_girls.find_by(girl_id: params[:girl_id])
   end
 end
