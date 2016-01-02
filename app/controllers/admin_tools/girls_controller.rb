69c5d7bd3f97e4cbaadc2484971c618611c15bb1
sert <demo.sert@gmail.com>
2016-01-02 07:19:00 +0000

    add show method

--- app/controllers/admin_tools/girls_controller.rb
+++ app/controllers/admin_tools/girls_controller.rb
@@ -1,7 +1,17 @@
 class AdminTools::GirlsController < ApplicationController
   before_action :authenticate_admin_user!
+  before_action :set_girl, only: [:show]
 
   def index
     @girls = Girl.all
+  end
+
+  def show
+  end
+
+  private
+
+  def set_girl
+    @girl = Girl.find params[:id]
   end
 end
